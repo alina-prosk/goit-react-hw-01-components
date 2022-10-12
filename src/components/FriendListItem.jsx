f0bde014aabe743dab9f545239cdabc83f2bf294
Alina Proskurnitska <proskur1987@gmail.com>
2022-10-12 10:09:40 +0300

    чистка

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -23,9 +23,3 @@ FriendListItem.propTypes = {
         })
     ),
 };
-
-/* <li class="item">
-  <span class="status"></span>
-  <img class="avatar" src="" alt="User avatar" width="48" />
-  <p class="name"></p>
-</li> */
